cf911d6ce11fbf6a8413aa097cb37eccbe043576
dev-smile <kou2004@naver.com>
2024-01-30 07:43:09 +0900

    feat: blogType 기본값 추가

--- api/index.js
+++ api/index.js
@@ -7,13 +7,14 @@ export default async (req, res) => {
   // type=daily or weekly
   // year=년도
   // blogType=velog || naver
-  const { name, type, year, blogType } = req.query;
+  const { name, type, year, blogType = "velog" } = req.query;
 
-    res.setHeader("Content-Type", "image/svg+xml");
-    try {
-        const post = blogType === "naver" 
-          ? await new NaverPostsFetcher(name).fetchPosts() 
-          : await fetchPosts(name);
+  res.setHeader("Content-Type", "image/svg+xml");
+  try {
+    const post =
+      blogType === "naver"
+        ? await new NaverPostsFetcher(name).fetchPosts()
+        : await fetchPosts(name);
 
     return res.send(createGrass(name, type, year, post, blogType));
   } catch (e) {
